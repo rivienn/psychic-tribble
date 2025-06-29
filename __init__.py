43afb633f07932d77e97ae1d5daf77b5c79f35e9
rivienn <avd.ilya2005@mail.ru>
2025-06-29 13:16:25 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from audio_processing.to_text import AudioToText
+from audio_processing.to_audio import TextToSpeech
+from audio_processing.vocal_selector import UVRSeparator
+
+
+__all__ = ["AudioToText",
+           "TextToSpeech",
+           "UVRSeparator"]
